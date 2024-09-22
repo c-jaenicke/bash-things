278d9f44260a4630aab9d0f51505cfbbfca8df93
c-jaenicke <me@chrisj.de>
2024-09-22 17:15:07 +0200

    improve setup-ssh-keys

--- setup-ssh-keys/setup-ssh-keys.sh
+++ setup-ssh-keys/setup-ssh-keys.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
-# Script to generate and upload keys to a system.
-# This script can utilise a YubiKey to generate a new key pair, the generated key will be saved on
-# the YubiKey.
+# Script to generate SSH keys and upload them to a system.
+# This script can utilize a YubiKey to generate a SSH key.
 #
 # NOTES:
-#
 # Restore Key on Different Host
 #   After setting up SSH on the YubiKey, you can regenerate the public key on different hosts.
 #   Regenerate the public key using `ssh-keygen -K`
 # 
 # Remove Authorized Key from Server:
 #   Edit the user-specific `authorized_keys` files in `/home/USER/.ssh`
-#   Remove the line corresponding to the key you want to delte
+#   Remove the line corresponding to the key you want to delete
 #
 # Setup SSH key for Git:
 #   Set git to use SSH keys to sign `git config --global gpg.format ssh`
@@ -23,8 +21,7 @@
 #   Sign a commit using `git commit -S -m "COMMIT MESSAGE"`
 #   Push a commit using `git push`
 #
-# MISC RESOURCES:
-#
+# RESOURCES:
 # https://developers.yubico.com/SSH/Securing_git_with_SSH_and_FIDO2.html
 # https://developers.yubico.com/SSH/Securing_SSH_with_FIDO2.html 
 # https://docs.github.com/en/authentication/managing-commit-signature-verification/telling-git-about-your-signing-key#telling-git-about-your-ssh-key
@@ -34,53 +31,18 @@
 ####################################################################################################
 # FUNCTIONS
 ####################################################################################################
-# generate a new key pair
-generate_key_pair () {
-    printf "########## 1. Create new key pair\n"
-
-    # get all values and confirmation
-    get_comment
-    get_type_of_key
-    get_filename "$type"
-
-    printf "##### Creating a new key pair using the values:\n\tUse Yuibkey: %s\n\tType: %s\n\tComment: %s\n" "$yubikey" "$type" "$comment"
-    printf "##### Saving files in %s/%s and %s/%s.pub\n" "$(pwd)" "$filename" "$(pwd)" "$filename"
-
-    # generate new key here
-    if [[ "$yubikey" == "y" ]]; then
-        # Source: https://developers.yubico.com/SSH/Securing_SSH_with_FIDO2.html (2024-09-19)
-        ssh-keygen -f "$filename" -C "$comment" -t "$type" -a 100 -O resident -O verify-required
-    else
-        ssh-keygen -f "$filename" -C "$comment" -t "$type" -a 100
-    fi
-    
-    # return filename here to use for uploading
-}
-
-# get the filename to save the new key under, if no input, use id_$type
-get_filename () {
-    read -r -p "##### (-f) Enter  file in which to save the key (default=\"id_$1\"): " filename
-    # if filename is empty, use default one
-    if [[ -z "$filename" ]]; then
-        filename="id_$1"
+# Get key_comment for key. Use empty if no input.
+get_input_comment () {
+    read -r -p "##### (-C) Enter the key_comment to add to the new key (default=empty): " key_comment
+    if [[ -z "$key_comment" ]]; then
+        key_comment=""
     fi
 }
 
-# get comment to add to key, if empty use empty
-get_comment () {
-    read -r -p "##### (-C) Enter the comment to add to the new key (default=\"\"): " comment
-    # if input is empty, use empty
-    if [[ -z "$comment" ]]; then
-        comment=""
-    fi
-}
-
-# get the type of key to create
-get_type_of_key () {
-    read -r -p "##### (-t) Enter the type of key you want to create (default=\"ed25519\") [Regular: ed25519 | rsa] [Yubikey: ed25519-sk]: " type
-    # if input is empty, use default type ed25519
+# Get type of key to generate. Use standard ed25519 key if no input and no YubiKey used. If YubiKey used, set type to ed25519-sk.
+get_input_type () {
+    read -r -p "##### (-t) Enter the type of key you want to create (default=ed25519 | YubiKey default=ed25519-sk) [Regular: ed25519 | rsa] [Yubikey: ed25519-sk]: " type
     if [[ -z "$type" ]]; then
-        # if empty, set to default YubiKey or regular type 
         if [[ "$yubikey" == "y" ]]; then
             type="ed25519-sk"
         else
@@ -89,48 +51,80 @@ get_type_of_key () {
     fi
 }
 
-# upload the key
+# Ask if user is using a YubiKey.
+get_input_yubikey () {
+    read -r -p "##### Are you using a Yubico Yubikey (with FIDO2) for this setup? If yes, plug it in and confirm! [y/N]: "
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        yubikey="y"
+    else
+        yubikey="n"
+    fi
+}
+
+# Get filename to save new key under. Use name based on type of key used it no input.
+get_input_filename () {
+    read -r -p "##### (-f) Enter  file in which to save the key (default=\"id_$1\"): " filename
+    if [[ -z "$filename" ]]; then
+        filename="id_$1"
+    fi
+}
+
+# Generate a new key pair with given values.
+generate_key () {
+    printf "########## 1. Create new key pair\n"
+    get_input_comment
+    get_input_yubikey
+    get_input_type
+    get_input_filename "$type"
+    printf "##### Creating a new key pair using the values:\n\tUse Yuibkey: %s\n\tType: %s\n\tComment: %s\n" "$yubikey" "$type" "$key_comment"
+    printf "##### Saving files in %s/%s and %s/%s.pub\n" "$(pwd)" "$filename" "$(pwd)" "$filename"
+
+    if [[ "$yubikey" == "y" ]]; then
+        # Source: https://developers.yubico.com/SSH/Securing_SSH_with_FIDO2.html (2024-09-19)
+        ssh-keygen -f "$filename" -C "$key_comment" -t "$type" -a 100 -O resident -O verify-required
+    else
+        ssh-keygen -f "$filename" -C "$key_comment" -t "$type" -a 100
+    fi
+}
+
+# Upload a key to a system.
 upload_key () {
-    # need username, ip and port for initial access
-    printf "########## 2. Upload the key\n"
+    printf "########## 2. Upload a key\n"
     read -r -p "##### Add key to remote host? [y/N]: " -n 1
     printf "\n"
     if [[ "$REPLY" =~ ^[Yy]$ ]]; then
         printf "##### Adding pub  key to  remote host\n"
-        
-        # if no filename given, ask user
+
+        # If -u|upload is called directly, no filename is set. Ask user for path to identity.
         if [[ -z "$filename" ]]; then
             read -r -p "##### Enter the path to the new identity: " filename
         fi
         
-        # read server data
+        # Get data for server.
         read -r -p "##### Enter username for remote host to  add key for: " username
         read -r -p "##### Enter ip for remote host to  add key for: " ip
         read -r -p "##### Enter ssh port on remote host: " port
-        
-        # copy key to server, ask if user needs to authenticate with an identity
-        read -r -p "Are you authenticating using an already existing and assigned identity? [y/N]: " -n 1
+
+        # Copy key to server. Ask if password login is possible, if not get existing identity for certificate auth.
+        read -r -p "##### Are you authenticating using an already existing and assigned identity? [y/N]: " -n 1
         printf "\n"
         if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-            # pass existing identity to ssh using -o
-            read -r -p "Enter the path to that identity: " pathtoidentity
+            # Existing identity gets passed to ssh using -o parameter.
+            read -r -p "##### Enter the path to that identity: " pathtoidentity
             ssh-copy-id -f -i "$filename" -o "IdentityFile $pathtoidentity" -p "$port" "$username"@"$ip"
         else
             ssh-copy-id -i "$filename" -p "$port" "$username"@"$ip"
         fi
 
-        # print steps after key as been added
         printf "##### Use 'ssh -i %s -p %s %s@%s' to connect to the remote host using the certificate.\n" "${filename/.pub/}" "$port" "$username" "$ip"
-        printf "##### Important notes:
-    Make sure to NOT use the .pub file of the key to authenticate!
-    Set the correct permissions using chmod: 
+        printf "##### Important notes:\n\tMake sure to NOT use the .pub file of the key to authenticate!\n\tSet the correct permissions using chmod:
         644 for %s (public key)
         600 for %s (private key)!\n" "$filename" "${filename/.pub/}"
         printf "##### Consider adding 'IdentityFile %s/%s' and 'IdentitiesOnly yes' to section of the remote host in your .ssh/config file.\n" "$(pwd)" "$filename"
     fi
 }
 
-# print a help string
+# Print a help string.
 print_help () {
     printf "########## Help:
     $ setup-ssh-keys [-h|help] [-g|gen] [-u|upload]
@@ -142,7 +136,7 @@ print_help () {
         1. Generates a new key pair, with optional Yubikey integration.
         2. Uploads the key to a specified serv
 
-    Make sure to plug your Yubikey in before using this script!\n\n"
+    Make sure to plug your Yubikey in before using this script!\n"
 }
 
 ####################################################################################################
@@ -154,7 +148,7 @@ case $1 in
         ;;
 
     -g|gen)
-        generate_key_pair
+        generate_key
         ;;
 
     -u|upload)
@@ -162,16 +156,7 @@ case $1 in
         ;;
 
     *)
-        print_help
-        read -r -p "##### Are you using a Yubico Yubikey (with FIDO2) for this setup? If yes, plug it in and confirm! [y/N]: "
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            yubikey="y"
-        else
-            yubikey="n"
-        fi
-
-        generate_key_pair
+        generate_key
         upload_key 
         ;;
 esac
-
